789bb4a4c0910a47dddd1d929de21f38503078d7
Alejandro Pena <adpena@gmail.com>
2019-07-18 21:08:31 -0500

    Preparing for PyPI - making revisions based on Test PyPI deployment

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Distributed computing for everyone in modern Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/adpena/achilles ",
+    url="https://github.com/adpena/achilles",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
